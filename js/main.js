d40cb776febc1f2fe716a0f8b52de0ee6b769964
A M <adsda@gmail.com>
2021-03-25 04:35:34 +0300

    New fixed bugs

--- js/main.js
+++ js/main.js
@@ -5,3 +5,50 @@ textarea.addEventListener('keyup', function(){
     this.style.height = this.scrollHeight + "px";
   }
 });
+
+
+
+
+
+
+
+// $('.herois__slider-pic').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   asNavFor: '.herois__slider-text',
+//   prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.svg" alt="prev"></button>',
+//   nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.svg" alt="next"></button>',
+//   responsive: [{
+//       breakpoint: 769,
+//       settings: {
+//           arrows: false
+//       }
+//   }]
+// });
+// $('.herois__slider-text').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   fade: true,
+//   asNavFor: '.herois__slider-pic',
+//   arrows: true
+// });
+
+
+$('.content-product__slider-pic').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: false,
+  fade: true,
+  asNavFor: '.content-product__slider-text'
+});
+$('.content-product__slider-text').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: '<button class="slick-btn slick-prev"><img src="img/svg/slider-arrow-left.svg" alt="prev"></button>',
+  nextArrow: '<button class="slick-btn slick-next"><img src="img/svg/slider-arrow-right.svg" alt="next"></button>',
+  dots: true,
+  fade: true,
+  asNavFor: '.content-product__slider-pic',
+});
+			
